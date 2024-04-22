46ef0329798922362bee28997dfc090dc62edba1
Reymond Bolambao <reymond.bolambao@churchillre.com>
2024-04-23 04:27:12 +0800

    Simple ecs task

--- index.php
+++ index.php
@@ -4,7 +4,24 @@ include('credentials.php');
 
 use Aws\Exception\AwsException;
 
-
+function add($num1, $num2){
+    $add = $num1 + $num2;
+    return $add;
+}
+ 
+if(isset($_SERVER['NUM1']) && isset($_SERVER['NUM2'])){
+    $num1 = $_SERVER['NUM1'];
+    $num2 = $_SERVER['NUM2'];
+}else{
+    $num1 = 5;
+    $num2 = 2;
+}
+ 
+$res = add($num1, $num2);
+$data = array('num1' => $num1, 'num2' => $num2, 'result' => $res);
+echo json_encode($data);
+return json_encode($data);
+exit();
 
 
 
